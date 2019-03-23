21b8b0e997e8031a608159bd1f4dac2ab62e6a74
Randy Jap <randyjap@gmail.com>
2019-03-23 23:29:37 +0800

    configure webpack with theme assets add basic theme stylesheets and images add root 'welcome' route, controller, and view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+require("vendors/theme")
+require("vendors/svgspriteinject")
