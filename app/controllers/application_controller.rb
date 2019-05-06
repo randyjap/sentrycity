a856491597a70c1ebc353500237563044c7bff8e
Randy Jap <randyjap@gmail.com>
2019-05-06 16:51:23 +0800

    add frontpage Chinese localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    cookies[:locale] = {
+      :value => locale,
+      :expires => 1.year.from_now,
+    }
+    I18n.locale = locale
+  end
+
   private
 
   def require_login
