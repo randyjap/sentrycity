0c8397140b0d0c0f603853d5f51b051877a014ab
Randy Jap <randyjap@gmail.com>
2019-05-27 23:35:16 +0800

    refactor

--- spec/factories/district.rb
+++ spec/factories/district.rb
@@ -1,5 +1,11 @@
+require('./db/districts')
+
 FactoryBot.define do
   factory :district do
-    name { "Name of District" }
+    sequence :name do |n|
+      raise if n >= DISTRICT_NAMES.count
+
+      DISTRICT_NAMES[n]
+    end
   end
 end
